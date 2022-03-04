2955ca868823516077a5dc99d6c6089f9ea1f4c3
Mark Hermano <marking.dav@gmail.com>
2022-03-04 13:26:31 +0800

    added navbar

--- src/layouts/AuthedLayout.vue
+++ src/layouts/AuthedLayout.vue
@@ -1,27 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-  </div>
-  <RouterView/>
+  <section id="authed-layout">
+    <Navbar />
+    <RouterView/>
+  </section>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
-  name: "AuthedLayout"
+  name: "AuthedLayout",
+  components: {
+    Navbar
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+#authed-layout {
+  margin: 0;
+  width: 100%;
+  background-image: url('~@/assets/images/bg-triangles.png');
+  background-size: cover;
+  height: 100vh;
+}
+
 #nav {
   padding: 30px;
-}
+  
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  a.router-link-exact-active {
+    color: #42b983;
+  }
 }
 </style>
